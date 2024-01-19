b2a148e34dca6f08eb2ecdde662d25d820800b99
Oh Olya <chanfoxx@yandex.ru>
2024-01-19 14:01:17 +0300

    edit function

--- src/main.py
+++ src/main.py
@@ -1,15 +1,29 @@
-from src import utils
+from src.utils import Operation
 
 
-# вводим переменную для файла с данными и
-# загружаем его с помощью функции.
 filename = 'operations.json'
-operations = utils.operations_file(filename)
+# Создаем экземпляр класса Operation, открываем json-файл.
+operations = Operation(filename)
+# Выбираем выполненные операции.
+ex_operations = operations.get_executed_operations()
+# Сортирует операции по дате и возвращает список пяти последних операций.
+sorted_operations = operations.get_sorted_operations(ex_operations)
 
-# получаем список 5-ти последних банковских операций.
-last_five_operations = utils.last_operations(operations)
+# Проходимся циклом по списку отсортированных операций и форматируем данные.
+for operation in sorted_operations:
+    # Форматируем дату в вид ДД.ММ.ГГГГ.
+    date = operations.get_format_date(operation['date'][:10])
+    # Выбираем описание банковской операции.
+    description = operation['description']
+    # Номер "от" кого перевод (если он виден) и
+    # номер "кому" перевод
+    from_card = operation.get('from', 'Unknown')
+    to_card = operation['to']
+    # Сумма и валюта перевода.
+    amount = operation['operationAmount']['amount']
+    currency = operation['operationAmount']['currency']['name']
+    # Маскируем номера счетов/карт.
+    masked_from = operations.mask_card_number(from_card)
+    masked_to = operations.mask_card_number(to_card)
 
-# выводим пользователю 5 выполненных последних операций.
-formatted_operation = utils.print_last_operations(last_five_operations)
-for operation in formatted_operation:
-    print(operation)
+    print(f'{date} {description}\n{masked_from} -> {masked_to}\n{amount} {currency}\n')
